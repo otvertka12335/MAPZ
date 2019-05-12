dd6d6198dd8b722de4b9b0130f713bd0274e89ab
Vadim <vkravchik@gmail.com>
2019-05-12 17:53:33 +0300

    Visitor

--- src/_22_Visitor/Main.java
+++ src/_22_Visitor/Main.java
@@ -1,4 +1,45 @@
 package _22_Visitor;
 
 public class Main {
+    public static void main(String[] args) {
+        People user = new SimpleVisitor();
+        user.doJob(new ConcreteVisitor());
+    }
+}
+
+interface People {
+    void doJob(Visitor visitor);
+}
+
+class SimpleVisitor implements People {
+    @Override
+    public void doJob(Visitor visitor) {
+        visitor.doSimple();
+    }
+}
+
+class Manager implements People {
+
+    @Override
+    public void doJob(Visitor visitor) {
+        visitor.doManager();
+    }
+}
+
+interface Visitor {
+    void doSimple();
+    void doManager();
+}
+
+class ConcreteVisitor implements Visitor {
+
+    @Override
+    public void doSimple() {
+        System.out.println("Look Film");
+    }
+
+    @Override
+    public void doManager() {
+        System.out.println("Look for security");
+    }
 }
